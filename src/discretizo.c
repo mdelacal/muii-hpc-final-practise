3bf78fb1bed3760424283dd3d7ec92cac9cbcb85
FelixAngelMartinez <FelixAngel.Martinez@alu.uclm.es>
2020-06-07 13:52:08 +0200

    Hybrid parallelism

--- src/discretizo.c
+++ src/discretizo.c
@@ -15,16 +15,25 @@
 #include <omp.h>
 
 /* CONSTANTES */
-#define VECTOR_SIZE 100000000 /* tamaño N del vector de edades */
 #define GRUPO_1 1             /* grupo de edad [0, 14] años */
 #define GRUPO_2 2             /* grupo de edad [15, 24] años */
 #define GRUPO_3 3             /* grupo de edad [25, 64] años */
 #define GRUPO_4 4             /* grupo de edad [65, 95] años */
 
+void checkintegrity(int vector_size ,int * grupo1, int * grupo2, int * grupo3, int * grupo4){
+    int suma_elementos_grupos = *grupo1 + *grupo2 + *grupo3 + *grupo4;
+    printf("Suma total es: %d\n",suma_elementos_grupos);
+    if(suma_elementos_grupos == vector_size){
+        printf("Integridad correcta\n");
+    }else{
+        printf("Integridad INCORRECTA\n");
+    }
+}
+
 int main()
 {
     /* VARIABLES */
-    long vector[] = {1000, 10000, 100000, 1000000, 10000000};
+    long vector[] = {1000000};
     int vector_size = sizeof(vector) / sizeof(vector[0]);
     long n;
     int i, actual_threads, k;
@@ -34,9 +43,9 @@ int main()
     unsigned char *solution;                /* vector con la solución, grupo al que pertenece cada edad */
     int group1, group2, group3, group4 = 0; /* elementos por grupo */
     /* Cores */
-    omp_set_dynamic(0);     /* Desactivamos el número de hilos dinámicos */
-    omp_set_num_threads(4); /* Seleccionamos el número de hilos */
-
+    omp_set_dynamic(0);                     /* Desactivamos el número de hilos dinámicos */
+    omp_set_num_threads(4);                 /* Seleccionamos el número de hilos */
+    omp_set_nested(1);                      /* Permite el anidamiento del paralelismo*/
     /* Time */
     double t_ini, t_fin;
     double secs;
@@ -46,78 +55,94 @@ int main()
         n = vector[k];
         v = (unsigned char *)malloc(sizeof(unsigned char) * n);
 
+        srand(time(NULL)); /* para generar elementos aleatorios diferentes */
+
+        /* Inicializamos el vector de N elementos de forma aleatoria,
+        con edades entre 0 y 95 años */
         #pragma omp parallel for
         for (i = 0; i < n; i++)
-        {                    
+        {
             v[i] = rand() % 96;
         }
+        /* printf("Tamaño vector aleatorio: %lu\n", (sizeof(v) / sizeof(v[0]))); */
+
         /* Probaremos con un conjunto de hilos */
-        for (actual_threads = 1; actual_threads <= total_threads; actual_threads++)
-        {          
-            /* Reservar espacio en memoria para los vectores v y solution */
-            
-            solution = (unsigned char *)malloc(sizeof(unsigned char) * n);
-            /* Determinar nº de threads y con cuantos elementos trabajará cada proceso */
-            omp_set_num_threads(actual_threads);
-            /*omp_set_num_threads(omp_get_num_threads());*/ /* si queremos utilizar todos los threads disponibles */
-            n_per_thread = n / actual_threads;
+        for (actual_threads = 2; actual_threads <= total_threads/2; actual_threads++)
+        {
+        /* Reservar espacio en memoria para los vectores v y solution */
 
-            srand(time(NULL)); /* para generar elementos aleatorios diferentes */
+        solution = (unsigned char *)malloc(sizeof(unsigned char) * n);
+        /* Determinar nº de threads y con cuantos elementos trabajará cada proceso */
+        omp_set_num_threads(actual_threads);
+        /*omp_set_num_threads(omp_get_num_threads());*/ /* si queremos utilizar todos los threads disponibles */
+        n_per_thread = n / actual_threads;        
+        /* printf("A cuantos hilos tocan: %d\n",n_per_thread); */
+        #pragma omp parallel num_threads(actual_threads)
+        {
+        t_ini = omp_get_wtime(); /* Inicio del contador de tiempo */       
 
+        /* Clasificación discretización por grupos de edades */
+        #pragma omp for private(i) schedule(static, n_per_thread)
+        for (i = 0; i < n; i++)
+        {        
+            /* PARALELIZAR */
             #pragma omp parallel num_threads(actual_threads)
-            {
-                t_ini = omp_get_wtime(); /* Inicio del contador de tiempo */
-                /* Inicializamos el vector de N elementos de forma aleatoria,
-                con edades entre 0 y 95 años */
-                /*#pragma omp for schedule(static, n_per_thread)*/
-                
-                /*#pragma omp barrier*/
-                /* Clasificación discretización por grupos de edades */
-                #pragma omp for private(i) schedule(static, n_per_thread)
-                for (i = 0; i < n; i++)
+            {   
+                /*printf("Número de hilos %d\n",omp_get_num_threads());*/
+                #pragma omp sections
                 {
-                    /*printf("Número de hilos %d\n",omp_get_num_threads());*/
-                    /* PARALELIZAR */
-                    if (v[i] <= 14)
+                    #pragma omp section
                     {
-                        solution[i] = GRUPO_1;
-                        group1++;
+                        if (v[i] <= 14)
+                        {
+                            solution[i] = GRUPO_1;
+                            group1++;
+                        }
+                        else if (v[i] >= 15 && v[i] <= 24)
+                        {
+                            solution[i] = GRUPO_2;
+                            group2++;
+                        }
                     }
-                    else if (v[i] >= 15 && v[i] <= 24)
+                    #pragma omp section
                     {
-                        solution[i] = GRUPO_2;
-                        group2++;
+                        if (v[i] >= 25 && v[i] <= 64)
+                        {
+                            solution[i] = GRUPO_3;
+                            group3++;
+                        }
+                        else if (v[i] >= 65)
+                        {
+                            solution[i] = GRUPO_4;
+                            group4++;
+                        }
                     }
-                    else if (v[i] >= 25 && v[i] <= 64)
-                    {
-                        solution[i] = GRUPO_3;
-                        group3++;
-                    }
-                    else
-                    {
-                        solution[i] = GRUPO_4;
-                        group4++;
-                    }
-                }
-                t_fin = omp_get_wtime(); /* Fin del contador de tiempo */
-            }
-            /* RESULTADOS */
-            printf("Iteracción para: %d hilos y un tamaño de vector de: %ld \n", actual_threads, n);
+                } /* Fin secciones */
+            } /* Fin second parallel */
+        } /* Fin for chunks */
+        t_fin = omp_get_wtime(); /* Fin del contador de tiempo */        
+        }   /* Fin first parallel */
 
-            printf("Grupo 1: %i elementos\n", group1);
-            printf("Grupo 2: %i elementos\n", group2);
-            printf("Grupo 3: %i elementos\n", group3);
-            printf("Grupo 4: %i elementos\n", group4);
+        /* COMPROBACIÓN INTEGRIDAD VECTOR RESULTANTE */ 
+        checkintegrity(n,&group1,&group2,&group3,&group4);
+        
+        /* RESULTADOS */
+        printf("Iteracción para: %d hilos y un tamaño de vector de: %ld \n", actual_threads, n);
 
-            /* Salida tiempo */
-            secs = (float)((t_fin - t_ini)* 1000.0); /* CLOCKS_PER_SEC */
-            printf("El tiempo estimado en realizar el trabajo es de: %.16g ms\n", secs);
+        printf("Grupo 1: %i elementos\n", group1);
+        printf("Grupo 2: %i elementos\n", group2);
+        printf("Grupo 3: %i elementos\n", group3);
+        printf("Grupo 4: %i elementos\n", group4);
 
-            /* Para finalizar, liberamos la memoria del vector v y solution */                        
-            free(solution);
+        /* Salida tiempo */
+        secs = (float)((t_fin - t_ini) * 1000.0); /* CLOCKS_PER_SEC */
+        printf("El tiempo estimado en realizar el trabajo es de: %.16g ms\n", secs);
+
+        /* Para finalizar, liberamos la memoria del vector v y solution */
+        free(solution);
         }
         printf("------------------------\n");
-        free(v);  
-    }
+        free(v);
+    }    
     return 0;
 }
